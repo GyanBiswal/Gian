dc8922dd605e4c5137f407ff9f6bf758c545af4e
Gyan <gyanbiswal@MacBookAir.bbrouter>
2026-03-25 21:12:10 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-green-400 text-4xl">
+      Tailwind Working 🚀
+    </div>
+  );
+}
